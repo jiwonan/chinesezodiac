08ae0bc1b0b5d3a03ed1d233bc17d08b6ce406dc
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-06 15:29:33 +0900

    2020.05.06 C# 실습문제 05

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,53 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            int year;
+
+            Console.Write("태어난 년도 입력: ");
+            year = int.Parse(Console.ReadLine());
+
+            switch (year % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이 띠");
+                    break;
+                case 1:
+                    Console.WriteLine("닭 띠");
+                    break;
+                case 2:
+                    Console.WriteLine("개 띠");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지 띠");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐 띠");
+                    break;
+                case 5:
+                    Console.WriteLine("소 띠");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이 띠");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼 띠");
+                    break;
+                case 8:
+                    Console.WriteLine("용 띠");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀 띠");
+                    break;
+                case 10:
+                    Console.WriteLine("말 띠");
+                    break;
+                case 11:
+                    Console.WriteLine("양 띠");
+                    break;
+                default:
+                    Console.WriteLine("띠가 없습니다.");
+                    break;
+            }
         }
     }
 }
